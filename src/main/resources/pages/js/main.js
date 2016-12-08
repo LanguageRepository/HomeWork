332ebee6915acbd530dfa564a1f7008602d7c607
LanguageRepository <vladyslav.bublyk@nure.ua>
2016-12-08 12:30:28 +0200

    Correct project

--- src/main/resources/pages/js/main.js
+++ src/main/resources/pages/js/main.js
@@ -6,15 +6,30 @@ function createAccount() {
     var password = document.getElementById("inputPassword").value;
     var result = {"username" : username,
                   "password": password};
-    $.ajax({
-        type: 'POST',
-        contentType: 'application/json',
-        url: '/account',
-        data: JSON.stringify(result),
-        dataType: 'json',
-        success: function () {
-            alert("User created");
-        }
-    });
-
+    if(isAccountValid()) {
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/account',
+            data: JSON.stringify(result),
+            dataType: 'json',
+            success: function (data) {
+                alert("User created");
+            }
+        });
+    } else {
+        alert("Заполните поля");
+    }
+}
+function isAccountValid() {
+    var result;
+    var checkUsername = document.getElementById('inputUsername').value.length;
+    var checkPassword = document.getElementById('inputPassword').value.length;
+    var checkName = document.getElementById('inputName').value.length;
+    if(checkUsername == 0 || checkPassword == 0 || checkName == 0) {
+        result = false;
+    } else {
+        result = true;
+    }
+    return result;
 }
