e09c5b9577456fab367e6f52211a635831597e6b
LanguageRepository <vladyslav.bublyk@nure.ua>
2016-12-08 11:33:57 +0200

    Correct project

---
+++ src/main/java/com/example/exception/HttpMessage.java
@@ -0,0 +1,33 @@
+package com.example.exception;
+
+public class HttpMessage {
+
+    private String status;
+
+    private String message;
+
+    public HttpMessage(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpMessage(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
